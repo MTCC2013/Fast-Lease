d035bf2d0df5923fb602da1eb4d7610306233955
Antônio <Antnio@Antonio>
2013-09-21 22:18:28 -0300

    Classe Area Usuario

---
+++ src/br/com/model/negocio/AreaUsuario.java
@@ -0,0 +1,26 @@
+package br.com.model.negocio;
+
+public class AreaUsuario {
+    
+    private int codigo;
+    private String area;
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+    
+    
+    
+}
